e77c3816f0405d5b08d6322f2cc767493d4aac71
Ricardo Gutierrez <ricardog.astocondor@gmail.com>
2022-12-21 00:51:26 +0100

    Changed employee controller unit test

--- employee/src/test/java/com/rga/employee/controller/EmployeeControllerTest.java
+++ employee/src/test/java/com/rga/employee/controller/EmployeeControllerTest.java
@@ -1,11 +1,5 @@
 package com.rga.employee.controller;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import com.rga.employee.dto.EmployeeDto;
 import com.rga.employee.model.Employee;
 import com.rga.employee.service.EmployeeService;
@@ -15,35 +9,37 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class EmployeeControllerTest {
 
-  @InjectMocks
-  private EmployeeController employeeController;
-  @Mock
-  private EmployeeService employeeService;
+    @InjectMocks
+    private EmployeeController employeeController;
+    @Mock
+    private EmployeeService employeeService;
 
-  @Test
-  void createNewEmployee_InputEmployeeData_EmployeeCreated() {
-    // given
-    EmployeeDto employeeDto = EmployeeDto.builder()
-        .firstname("Ricardo")
-        .lastname("gutierrez")
-        .active(true)
-        .build();
-    Employee employee = Employee.builder()
-        .firstname("Ricardo")
-        .lastname("gutierrez")
-        .active(true)
-        .build();
+    @Test
+    void createNewEmployee_InputEmployeeData_EmployeeCreated() {
+        // given
+        EmployeeDto employeeDto = EmployeeDto.builder()
+                .firstname("Ricardo")
+                .lastname("gutierrez")
+                .active(true)
+                .build();
+        Employee employee = Employee.builder()
+                .firstname("Ricardo")
+                .lastname("gutierrez")
+                .active(true)
+                .build();
 
-    // when
+        // when
+        when(employeeService.create(employeeDto)).thenReturn(employee);
+        Employee createdEmployee = employeeController.createNewEmployee(employeeDto).getBody();
 
-    when(employeeService.create(employeeDto)).thenReturn(employee);
-    Employee createdEmployee = employeeService.create(employeeDto);
-
-    // then
-    assertNotNull(createdEmployee);
-    verify(employeeService, times(1)).create(any());
-  }
+        // then
+        assertEquals(createdEmployee, employee);
+        verify(employeeService, times(1)).create(any());
+    }
 }
